7748255405f2ab7f30e28cf2452d364d311b84b6
jaeil <59818827+jaeilssss@users.noreply.github.com>
2021-05-11 11:12:24 +0900

    ing

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const functions = require("firebase-functions");
-
-exports.helloWorld2 = functions.https.onCall((data) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  const text = data.text;
-  functions.logger.info("text logs!", text);
-  return {text: "Hello from Firebase!"};
+const {RtcTokenBuilder, RtcRole} = require("agora-access-token");
+const APP_ID = "65b9a66d547a48f78cb3168e17d6867b";
+const APP_CERTIFICATE = "fb313b9dc3254c72acb28ca1264d8295";
+exports.generateToken = functions.https.onCall((data)=>{
+  const channelName = data.channelName;
+  const uid = data.uid;
+  const role = RtcRole.SUBSCRIBER;
+  const expireTime = data.expireTime;
+  const currentTime = Math.floor(Date.now() / 1000);
+  const privilegeExpireTime = currentTime + expireTime;
+  const token = RtcTokenBuilder.buildTokenWithUid(APP_ID, APP_CERTIFICATE,
+      channelName, uid, role, privilegeExpireTime);
+  return {"token": token};
 });
