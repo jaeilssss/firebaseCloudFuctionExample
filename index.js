8ea7e98af8ae88004b38889283f44ee16390801a
Jaeilssss <59818827+jaeilssss@users.noreply.github.com>
2021-05-11 16:18:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const {RtcTokenBuilder, RtcRole} = require("agora-access-token");
-const APP_ID = "65b9a66d547a48f78cb3168e17d6867b";
-const APP_CERTIFICATE = "fb313b9dc3254c72acb28ca1264d8295";
+const APP_ID = "";
+const APP_CERTIFICATE = "5";
 exports.generateToken = functions.https.onCall((data)=>{
   const channelName = data.channelName;
   const uid = data.uid;
